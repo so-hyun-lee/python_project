48541af1865ab7a82cce08f2b96f814dda94c1eb
so-hyun-lee <sxhxxxlxx@gmail.com>
2020-09-14 22:26:35 +0900

    commit2

--- mac_git_practice.py
+++ mac_git_practice.py
@@ -3,4 +3,12 @@ Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27)
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print(3+4)
 7
->>> 
+>
+len = int(input())
+for i in range(1, len+1):
+    a= input()
+    n=list(map(int, input().split()))
+    print("#{}{}".format(i,max(n)-min(n))
+      
+          
+          
